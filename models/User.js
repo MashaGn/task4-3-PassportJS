b6571ae87de3344101234e2349af02759e6fea3e
Mary <mary.langeclair@gmail.com>
2020-04-23 09:30:21 +0300

    first

---
+++ models/User.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require('../conn');
+
+const UserSchema = new Schema({
+  "login": {
+    "type": "String"
+  },
+  "password": {
+    "type": "String"
+  }
+
+},
+);
+
+const u = model('User', UserSchema);
+
+module.exports = { u };
